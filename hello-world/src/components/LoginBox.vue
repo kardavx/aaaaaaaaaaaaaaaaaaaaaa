479827ac1c5c8e601cbcefad66d7d40016411454
kardavx <kardaszjanojutubero@gmail.com>
2022-08-04 09:53:59 +0200

    Wybieranie api z array, css dla kafelkow i pare innych rzeczy nw nie chce mi sie pisac

--- hello-world/src/components/LoginBox.vue
+++ hello-world/src/components/LoginBox.vue
@@ -1,45 +1,59 @@
 <template>
   <div id="mainbox">
-    <select id="select1" v-model="selected">
-      <option disabled value="">wybierz jedno</option>
-      <option>Opcja 1</option>
-      <option>Opcja 2</option>
-      <option>Opcja 3</option>
-    </select>
     <div id="tiles">
-      <div id="tile">
-        {{GetName}}
-      </div>
-      <div id="tile">
-        {{GetName}}
-      </div>
-      <div id="tile">
-        {{GetName}}
-      </div>
+      <div id="tile">{{ GetName }}</div>
+      <div id="tile">{{ GetName }}</div>
+      <div id="tile">{{ GetName }}</div>
     </div>
   </div>
 </template>
 
 <script>
 
- import { mapActions } from "vuex"
+/* eslint-disable no-unused-vars */
+
+
+import { mapActions, mapState } from "vuex"
 
 export default {
   name: 'LoginBox',
+
   data() {
     return {
-      selected: "Opcja 1"
+      selected: "Opcja 1",
     }
   },
+
+  created() {
+    this.metoda("name","names")
+  },
+
   computed: {
+    ...mapState({
+      names: state => state.names
+    }),
+    GetNames() {
+      return this.$store.state.test.names
+    },
     GetName() {
-      return this.metoda("name")
+      const names = this.GetNames
+      console.log(names)
+      if (!names) {console.log("names empty"); return  "Ladowanie danych..."}
+      if (names < 1 && names[1] != "object Promise") {
+        return "Ladowanie danych..."
+      }
+      console.log(names)
+      return names[Math.floor(Math.random() * names.length)]
     }
   },
+
   methods: {
+    // ...mapState ({
+    //   names: state => state.names
+    // }),
      ...mapActions ({
        metoda: 'test/GetStringFromAPI'
-     })
+     }),
   }
 }
 
@@ -48,23 +62,38 @@ export default {
 <style scoped>
 
 #mainbox {
-  width: 35vw;
+  width: 70vw;
   height: 35vw;
   position: absolute;
-  background-color: lightgray;
-  box-shadow: 1px 1px 2px gray, 0 0 25px gray, 0 0 5px lightgray;
+  background-color: white;
+  box-shadow: 1px 1px 2px lightgray, 0 0 25px lightgray, 0 0 5px lightgray;
   transform: translate(-50%, -50%);
   top: 50%;
   left: 50%;
   border-radius: 10px 60px 15px 60px;
 }
 
-#select1 {
+#tiles {
+  margin: 2px auto;
+  width: 100%;
+  height:100%;
+}
+
+#tile {
+  width: calc(100% / 3);
+  display: inline-block;
+  height: 100%;
+  text-align: center;
+  background-color: white;
+  box-shadow: 1px 1px 2px lightgray, 0 0 25px lightgray, 0 0 5px lightgray;
+  border-radius: 60px 60px 60px 60px;
+}
+
+span {
   position: absolute;
   transform: translate(-50%, -50%);
   top:50%;
-  left: 50%;
-
+  left:50%;
 }
 
 </style>
--- hello-world/src/modules/ExampleModules.js
+++ hello-world/src/modules/ExampleModules.js
@@ -1,17 +1,41 @@
 const axios = require('axios').default;
 
+/* eslint-disable no-unused-vars */
+
+function GetRandomFromArray(array) {
+    return array[Math.floor(Math.random() * array.length)]
+}
+
 export default {
     namespaced: true,
     state: {
         count: 0,
+        names: [],
+        defaultapi: "https://swapi.dev/api/people/1",
+        apilist: ["https://swapi.dev/api/people/1", "https://swapi.dev/api/people/2", "https://swapi.dev/api/people/3"]
     },
     actions: {
-        GetStringFromAPI(_, second) {
-            axios.get(`https://swapi.dev/api/people/1`)
+        GetStringFromAPI({ state }, lookinjson, arraytoinsert) {
+            console.log(lookinjson + " function fired")
+            let ApiChoosen = GetRandomFromArray(state.apilist)
+            console.log(ApiChoosen)
+            if (ApiChoosen == null) {
+                ApiChoosen = state.defaultapi
+            }
+            console.log("knock to api")
+            axios.get(ApiChoosen)
                 .then(function (response) {
+                    console.log("got the response, parsing further")
                     const obj = response.data
-                    if (obj[second]) {
-                        return obj[second]
+                    if (obj[lookinjson]) {
+                        console.log("formatting the json table and parsing that back")
+                        if (state.names) {
+                            console.log("found")
+                            console.log(state.apilist.indexOf(ApiChoosen))
+                            state.names.push(obj[lookinjson])
+                            console.log(state.names)
+                        }
+                        //return obj[second]
                     }
                 });
         }
