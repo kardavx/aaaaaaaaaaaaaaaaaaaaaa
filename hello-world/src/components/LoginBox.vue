bd9b78b449a64c4f626f383e0845b91291143e7d
kardavx <kardaszjanojutubero@gmail.com>
2022-08-04 14:03:52 +0200

    kolejne zmiany

--- hello-world/src/components/LoginBox.vue
+++ hello-world/src/components/LoginBox.vue
@@ -1,49 +1,90 @@
 <template>
   <div id="mainbox">
-    <div id="tiles">
-      <div id="tile">{{ GetName() }}</div>
-      <div id="tile">{{ GetName() }}</div>
-      <div id="tile">{{ GetName() }}</div>
+    <div class="tiles" v-if="GetList">
+      <div class="tile" v-for="n in GetList" :key="n">
+          <span id="name">{{ n.name }}</span>
+          <span id="age">{{ n.birth_year }}</span>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 
-/* eslint-disable no-unused-vars */
-
 import { mapActions, mapState } from "vuex"
 
 export default {
   name: 'LoginBox',
 
+  data() {
+    return {
+      amountoftiles: 3
+    }
+  },
+
   created() {
-    this.metoda("name","names")
+    this.metoda()
   },
 
   computed: {
     ...mapState({
-      names: state => state.test.names
+      people: state => state.test.names,
     }),
+    GetList() {
+      console.log(this.people)
+      if (!this.people) {console.log("names empty"); return []}
+      let LocalList = []
+      console.log(this.people)
+      for (let i = 0; i < this.amountoftiles; i++) {
+        LocalList.push(this.people[Math.floor(Math.random() * this.people.length)])
+      }
+      console.log(LocalList)
+      return LocalList
+    }
   },
 
   methods: {
-    // ...mapState ({
-    //   names: state => state.names
-    // }),
      ...mapActions ({
        metoda: 'test/GetStringFromAPI'
      }),
-    GetName() {
-      console.log(this.names)
-
-      if (!this.names) {console.log("names empty"); return  "Ladowanie danych..."}
-      if (this.names < 1 && this.names[1] != "object Promise") {
-        return "Ladowanie danych..."
-      }
-      console.log(this.names)
-      return this.names[Math.floor(Math.random() * this.names.length)]
-    }
+    // GetName() {
+    //   console.log(this.names)
+    //
+    //   if (!this.names) {console.log("names empty"); return  "Ladowanie danych..."}
+    //   if (this.names < 1 && this.names[1] != "object Promise") {
+    //     return "Ladowanie danych..."
+    //   }
+    //   console.log(this.names)
+    //   return this.names[Math.floor(Math.random() * this.names.length)]
+    // },
+    // GetCorrectName(corr_id) {
+    //   const divs = document.querySelectorAll(".tile");
+    //   for (let i = 0; i < divs.length; i++) {
+    //     if (divs[i].id == corr_id) {
+    //       for (let x = 0; x < divs[i].children.length; x++) {
+    //
+    //         if (divs[i].children[x].id == "name") {
+    //
+    //           return console.log(divs[i].children[x].textContent)
+    //         }
+    //       }
+    //       // console.log(divs[i].name.textContent)
+    //       // return divs[i].name.textContent
+    //     }
+    //   }
+    // },
+    // GetAge(corr_id) {
+    //   console.log(this.birth_year)
+    //
+    //   if (!this.birth_year) {console.log("names empty"); return  "Ladowanie danych..."}
+    //   if (this.birth_year < 1 && this.birth_year[1] != "object Promise") {
+    //     return "Ladowanie danych..."
+    //   }
+    //   console.log(this.birth_year)
+    //   console.log(this.names.indexOf(document.getElementById('name').textContent))
+    //   this.GetCorrectName(corr_id)
+    //  // return this.birth_year[this.names.indexOf(document.getElementById('name').textContent)]
+    // }
   }
 }
 
@@ -63,13 +104,13 @@ export default {
   border-radius: 10px 60px 15px 60px;
 }
 
-#tiles {
+.tiles {
   margin: 2px auto;
   width: 100%;
   height:100%;
 }
 
-#tile {
+.tile {
   width: calc(100% / 3);
   display: inline-block;
   height: 100%;
@@ -80,10 +121,19 @@ export default {
 }
 
 span {
+  text-align: center;
+}
+
+#name {
   position: absolute;
-  transform: translate(-50%, -50%);
+  transform: translate(-50%, 0%);
   top:50%;
-  left:50%;
+}
+
+#age {
+  position: absolute;
+  transform: translate(-55%, 0%);
+  top:55%;
 }
 
 </style>
--- hello-world/src/modules/ExampleModules.js
+++ hello-world/src/modules/ExampleModules.js
@@ -2,42 +2,28 @@ const axios = require('axios').default;
 
 /* eslint-disable no-unused-vars */
 
-function GetRandomFromArray(array) {
-    return array[Math.floor(Math.random() * array.length)]
-}
-
 export default {
     namespaced: true,
     state: {
         count: 0,
-        names: [],
-        defaultapi: "https://swapi.dev/api/people/1",
-        apilist: ["https://swapi.dev/api/people/1", "https://swapi.dev/api/people/2", "https://swapi.dev/api/people/3"]
+        people: [],
     },
     actions: {
-        GetStringFromAPI({ state }, lookinjson, arraytoinsert) {
-            console.log(lookinjson + " function fired")
-            let ApiChoosen = GetRandomFromArray(state.apilist)
-            console.log(ApiChoosen)
-            if (ApiChoosen == null) {
-                ApiChoosen = state.defaultapi
-            }
+        GetStringFromAPI({ commit }) {
             console.log("knock to api")
-            axios.get(ApiChoosen)
+            axios.get('https://swapi.dev/api/people')
                 .then(function (response) {
                     console.log("got the response, parsing further")
-                    const obj = response.data
-                    if (obj[lookinjson]) {
-                        console.log("formatting the json table and parsing that back")
-                        if (state.names) {
-                            console.log("found")
-                            console.log(state.apilist.indexOf(ApiChoosen))
-                            state.names.push(obj[lookinjson])
-                            console.log(state.names)
-                        }
-                        //return obj[second]
-                    }
+                    const results = response.data.results
+                    const people = results.map(obj => obj)
+                    commit('SetPeople', people)
                 });
         }
     },
+    mutations: {
+        SetPeople(state, val) {
+            state.people = val
+            console.log(state.people)
+        },
+    }
 }
